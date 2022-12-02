46d32ace2ce09a95d2211c535d04986a8bfa4d07
Lewis Nelson <lewis@lewnelson.com>
2022-12-02 14:54:36 +0000

    day 1 part 1

--- src/days/1_dayOne/index.test.ts
+++ src/days/1_dayOne/index.test.ts
@@ -1,12 +1,55 @@
 import * as dayOne from './';
 import { TestCase } from '~/types';
 
-describe('dayOne', () => {
-  const testCases: TestCase[] = [
-    { description: 'not implemented', input: '', expectedOutput: 'not implemented' },
-  ];
+const inputA = `1000
+2000
+3000
 
+4000
+
+5000
+6000
+
+7000
+8000
+9000`;
+
+const inputB = '';
+
+const inputC = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+
+5000
+6000`;
+
+describe('dayOne', () => {
   describe('partOne', () => {
+    const testCases: TestCase[] = [
+      {
+        description: 'Where the last elf is carrying the most at 24000',
+        input: inputA,
+        expectedOutput: 24000,
+      },
+      {
+        description: 'When there is no input the output is 0',
+        input: inputB,
+        expectedOutput: 0,
+      },
+      {
+        description: 'Where more than one elf has the same amount of calories and they are carrying the most',
+        input: inputC,
+        expectedOutput: 11000,
+      },
+    ];
+
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayOne.partOne(input)).toBe(expectedOutput);
     });
--- src/days/1_dayOne/index.ts
+++ src/days/1_dayOne/index.ts
@@ -2,5 +2,7 @@
 import { Main } from '~/types';
 
 export const partOne: Main = input => {
-  return 'not implemented';
+  const elves = input.split('\n\n').map(elf => elf.split('\n').reduce((acc, curr) => acc + parseInt(curr), 0));
+  elves.sort((a, b) => a > b ? 1 : a < b ? -1 : 0);
+  return isNaN(elves[elves.length - 1]) ? 0 : elves[elves.length - 1];
 };
