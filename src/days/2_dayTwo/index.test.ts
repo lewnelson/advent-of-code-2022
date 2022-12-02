3a48349387edc5037cece27dc1d8b81e64b290db
Lewis Nelson <lewis@lewnelson.com>
2022-12-02 16:05:15 +0000

    day 2 part 2

--- src/days/2_dayTwo/index.test.ts
+++ src/days/2_dayTwo/index.test.ts
@@ -8,22 +8,41 @@ C Z`;
 const inputB = '';
 
 describe('dayTwo', () => {
-  const testCases: TestCase[] = [
-    {
-      description: 'When the total score should be 15',
-      input: inputA,
-      expectedOutput: 15,
-    },
-    {
-      description: 'When no games are played',
-      input: inputB,
-      expectedOutput: 0,
-    },
-  ];
-
   describe('partOne', () => {
+    const testCases: TestCase[] = [
+      {
+        description: 'When the total score should be 15',
+        input: inputA,
+        expectedOutput: 15,
+      },
+      {
+        description: 'When no games are played',
+        input: inputB,
+        expectedOutput: 0,
+      },
+    ];
+
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayTwo.partOne(input)).toBe(expectedOutput);
+    });
+  });
+
+  describe('partTwo', () => {
+    const testCases: TestCase[] = [
+      {
+        description: 'When the total score should be 12',
+        input: inputA,
+        expectedOutput: 12,
+      },
+      {
+        description: 'When no games are played',
+        input: inputB,
+        expectedOutput: 0,
+      },
+    ];
+
+    test.each(testCases)('$description', ({ input, expectedOutput }) => {
+      expect(dayTwo.partTwo(input)).toBe(expectedOutput);
     });
   });
 });
--- src/days/2_dayTwo/index.ts
+++ src/days/2_dayTwo/index.ts
@@ -1,75 +1,108 @@
 // https://adventofcode.com/2022/day/2
 import { Main } from '~/types';
 
+type PlayerOneHand = 'A' | 'B' | 'C';
+type PlayerTwoHand = 'X' | 'Y' | 'Z';
+
 const ROCK = 1;
 const PAPER = 2;
 const SCISSORS = 3;
 
-type Hand = typeof ROCK | typeof PAPER | typeof SCISSORS;
-type Result = 'draw' | 'playerOne' | 'playerTwo';
-
-const hands = {
+const playerOneHandMap = {
   A: ROCK,
   B: PAPER,
   C: SCISSORS,
-  X: ROCK,
-  Y: PAPER,
-  Z: SCISSORS,
 };
 
-const scores = {
-  lose: 0,
-  draw: 3,
-  win: 6,
+const WIN = 6;
+const DRAW = 3;
+const LOSE = 0;
+
+type Hand = typeof ROCK | typeof PAPER | typeof SCISSORS;
+type Outcome = typeof WIN | typeof DRAW | typeof LOSE;
+
+interface Scores {
+  playerOne: number;
+  playerTwo: number;
 }
 
-const getResult = (playerOne: Hand, playerTwo: Hand): Result => {
-  if (playerOne === playerTwo) {
-    return 'draw';
-  }
-
-  if (playerOne === ROCK && playerTwo === SCISSORS) {
-    return 'playerOne';
-  }
-
-  if (playerOne === PAPER && playerTwo === ROCK) {
-    return 'playerOne';
-  }
-
-  if (playerOne === SCISSORS && playerTwo === PAPER) {
-    return 'playerOne';
-  }
-
-  return 'playerTwo';
+const outcomes = {
+  [ROCK]: {
+    [WIN]: PAPER,
+    [DRAW]: ROCK,
+    [LOSE]: SCISSORS,
+  },
+  [PAPER]: {
+    [WIN]: SCISSORS,
+    [DRAW]: PAPER,
+    [LOSE]: ROCK,
+  },
+  [SCISSORS]: {
+    [WIN]: ROCK,
+    [DRAW]: SCISSORS,
+    [LOSE]: PAPER,
+  },
 };
 
-const getScores = (input: string): number[] => {
-  const lines = input.split('\n');
-  let playerOneScore = 0;
-  let playerTwoScore = 0;
-  lines.forEach(line => {
-    if (!line) return;
-    const [playerOne, playerTwo] = line.split(' ').map(hand => hands[hand as keyof typeof hands] as Hand);
-    playerOneScore += playerOne;
-    playerTwoScore += playerTwo;
+const getScores = (playerOne: Hand, playerTwo: Hand): Scores => {
+  const scores: Scores = {
+    playerOne,
+    playerTwo,
+  };
 
-    const result = getResult(playerOne, playerTwo);
-    if (result === 'draw') {
-      playerOneScore += scores.draw;
-      playerTwoScore += scores.draw;
-    } else if (result === 'playerOne') {
-      playerOneScore += scores.win;
-      playerTwoScore += scores.lose;
-    } else if (result === 'playerTwo') {
-      playerOneScore += scores.lose;
-      playerTwoScore += scores.win;
-    }
-  });
+  if (outcomes[playerOne][WIN] === playerTwo) {
+    scores.playerTwo += WIN;
+  } else if (outcomes[playerOne][DRAW] === playerTwo) {
+    scores.playerOne += DRAW;
+    scores.playerTwo += DRAW;
+  } else {
+    scores.playerOne += WIN;
+  }
 
-  return [playerOneScore, playerTwoScore];
+  return scores;
 };
 
 export const partOne: Main = input => {
-  const [_, myScore] = getScores(input);
-  return myScore;
+  const playerTwoHandMap = {
+    X: ROCK,
+    Y: PAPER,
+    Z: SCISSORS,
+  };
+
+  const scores = input.split('\n').reduce((acc, line) => {
+    if (!line) return acc;
+    const [playerOne, playerTwo] = line.split(' ') as [PlayerOneHand, PlayerTwoHand];
+    const playerOneShape = playerOneHandMap[playerOne] as Hand;
+    const playerTwoShape = playerTwoHandMap[playerTwo] as Hand;
+    const lineScores = getScores(playerOneShape, playerTwoShape);
+    return {
+      playerOne: acc.playerOne + lineScores.playerOne,
+      playerTwo: acc.playerTwo + lineScores.playerTwo,
+    };
+  }, { playerOne: 0, playerTwo: 0 } as Scores);
+
+  return scores.playerTwo;
+};
+
+export const partTwo: Main = input => {
+  const playerTwoOutcomeMap = {
+    X: LOSE,
+    Y: DRAW,
+    Z: WIN,
+  };
+
+  const scores = input.split('\n').reduce((acc, line) => {
+    if (!line) return acc;
+    const [playerOne, playerTwo] = line.split(' ') as [PlayerOneHand, PlayerTwoHand];
+    const playerOneShape = playerOneHandMap[playerOne] as Hand;
+    const playerTwoOutcome = playerTwoOutcomeMap[playerTwo] as Outcome;
+    const playerTwoShape = outcomes[playerOneShape][playerTwoOutcome] as Hand;
+    const lineScores = getScores(playerOneShape, playerTwoShape);
+    return {
+      playerOne: acc.playerOne + lineScores.playerOne,
+      playerTwo: acc.playerTwo + lineScores.playerTwo,
+    };
+  }, { playerOne: 0, playerTwo: 0 } as Scores);
+
+  return scores.playerTwo;
 };
