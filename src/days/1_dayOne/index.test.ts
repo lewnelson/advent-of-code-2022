3ede20170df527ed3c823ce21a53a8d90b84c461
Lewis Nelson <lewis@lewnelson.com>
2022-12-02 15:05:03 +0000

    day 1 part 2

--- src/days/1_dayOne/index.test.ts
+++ src/days/1_dayOne/index.test.ts
@@ -12,7 +12,9 @@ const inputA = `1000
 
 7000
 8000
-9000`;
+9000
+
+10000`;
 
 const inputB = '';
 
@@ -52,6 +54,30 @@ describe('dayOne', () => {
 
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayOne.partOne(input)).toBe(expectedOutput);
+    });
+  });
+
+  describe('partTwo', () => {
+    const testCases: TestCase[] = [
+      {
+        description: 'Where the combined total of the top three elves is 45000',
+        input: inputA,
+        expectedOutput: 45000,
+      },
+      {
+        description: 'When there is no input the output is 0',
+        input: inputB,
+        expectedOutput: 0,
+      },
+      {
+        description: 'Where more than one elf has the same amount of calories and they are carrying the most the combined total for the top three elves is 29000',
+        input: inputC,
+        expectedOutput: 29000,
+      },
+    ];
+
+    test.each(testCases)('$description', ({ input, expectedOutput }) => {
+      expect(dayOne.partTwo(input)).toBe(expectedOutput);
     });
   });
 });
--- src/days/1_dayOne/index.ts
+++ src/days/1_dayOne/index.ts
@@ -1,8 +1,25 @@
 // https://adventofcode.com/2022/day/1
 import { Main } from '~/types';
 
+const getElvesTotalCaloriesSorted = (input: string): number[] => {
+  const caloriesPerElf = input.split('\n\n').map(elf => elf.split('\n').reduce((acc, curr) => {
+    const n = parseInt(curr, 10);
+    if (isNaN(n)) return acc;
+    return acc + n;
+  }, 0));
+
+  caloriesPerElf.sort((a, b) => a > b ? -1 : a < b ? 1 : 0);
+  return caloriesPerElf;
+};
+
+const sum = (acc: number, curr: number) => acc + curr;
+
 export const partOne: Main = input => {
-  const elves = input.split('\n\n').map(elf => elf.split('\n').reduce((acc, curr) => acc + parseInt(curr), 0));
-  elves.sort((a, b) => a > b ? 1 : a < b ? -1 : 0);
-  return isNaN(elves[elves.length - 1]) ? 0 : elves[elves.length - 1];
+  const caloriesPerElf = getElvesTotalCaloriesSorted(input);
+  return caloriesPerElf.slice(0, 1).reduce(sum, 0);
+};
+
+export const partTwo: Main = input => {
+  const caloriesPerElf = getElvesTotalCaloriesSorted(input);
+  return caloriesPerElf.slice(0, 3).reduce(sum, 0);
 };
