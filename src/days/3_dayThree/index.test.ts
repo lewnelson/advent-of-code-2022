c2b0910c9eb67692c07f54f0fea8cfca8d217475
Lewis Nelson <lewis@lewnelson.com>
2022-12-03 12:47:43 +0000

    day 3 part 2

--- src/days/3_dayThree/index.test.ts
+++ src/days/3_dayThree/index.test.ts
@@ -15,7 +15,6 @@ jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw
 vjAwRtyoiPvj`;
 
 describe('dayThree', () => {
@@ -23,11 +22,23 @@ describe('dayThree', () => {
     const testCases: TestCase[] = [
       { description: 'For the given rucksacks the total is 157', input: inputA, expectedOutput: 157 },
       { description: 'For no input the total is 0', input: inputB, expectedOutput: 0 },
-      { description: 'When a rucksack has more than one common character only the first common character is used', input: inputC, expectedOutput: 179 },
+      { description: 'When a rucksack has more than one common character only the first common character is used', input: inputC, expectedOutput: 160 },
     ];
 
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayThree.partOne(input)).toBe(expectedOutput);
     });
   });
+
+  describe('partTwo', () => {
+    const testCases: TestCase[] = [
+      { description: 'For the given rucksacks the group badge total is 70', input: inputA, expectedOutput: 70 },
+      { description: 'For no input the total is 0', input: inputB, expectedOutput: 0 },
+      { description: 'When a group has no common character', input: inputC, expectedOutput: 18 },
+    ];
+
+    test.each(testCases)('$description', ({ input, expectedOutput }) => {
+      expect(dayThree.partTwo(input)).toBe(expectedOutput);
+    });
+  })
 });
--- src/days/3_dayThree/index.ts
+++ src/days/3_dayThree/index.ts
@@ -1,15 +1,23 @@
 // https://adventofcode.com/2022/day/3
 import { Main } from '~/types';
 
-const findCommonCharacters = (a: string, b: string): string[] => {
+const findCommonCharactersInTwoStrings = (a: string, b: string): string[] => {
   const aChars = a.split('');
   const bChars = b.split('');
   const commonChars = aChars.filter((char) => bChars.includes(char));
   return commonChars;
 };
 
-const findFirstCommonCharacter = (a: string, b: string): string | undefined => {
-  return findCommonCharacters(a, b)[0];
+const findCommonCharacters = (...strings: string[]): string[] => {
+  const [firstString, secondString, ...restOfStrings] = strings;
+  const commonChars = findCommonCharactersInTwoStrings(firstString, secondString);
+  return restOfStrings.reduce((commonChars, string) => {
+    return findCommonCharactersInTwoStrings(commonChars.join(''), string);
+  }, commonChars);
+};
+
+const findFirstCommonCharacter = (...strings: string[]): string | undefined => {
+  return findCommonCharacters(...strings)[0];
 };
 
 const getNumericalRepresentation = (char: string): number => {
@@ -23,10 +31,26 @@ export const partOne: Main = input => {
     if (!line) return sum;
     const [compartmentOne, compartmentTwo] = [line.slice(0, line.length / 2), line.slice(line.length / 2)];
     const commonCharacter = findFirstCommonCharacter(compartmentOne, compartmentTwo);
-    if (!commonCharacter) {
-      return sum;
+    if (!commonCharacter) return sum;
+    const n = getNumericalRepresentation(commonCharacter);
+    return sum + n;
+  }, 0);
+};
+
+export const partTwo: Main = input => {
+  const groups = input.split('\n').reduce((groups, line) => {
+    if (!line) return [];
+    const currentGroup = groups[groups.length - 1];
+    if (!currentGroup || currentGroup.length === 3) {
+      return [...groups, [line]];
     }
 
+    return [...groups.slice(0, groups.length - 1), [...currentGroup, line]];
+  }, [] as string[][]);
+
+  return groups.reduce((sum, group) => {
+    const commonCharacter = findFirstCommonCharacter(...group);
+    if (!commonCharacter) return sum;
     const n = getNumericalRepresentation(commonCharacter);
     return sum + n;
   }, 0);
