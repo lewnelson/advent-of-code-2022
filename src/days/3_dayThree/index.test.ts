0f8053eeb902dce2d68aef32e8bd5e2e51c51732
Lewis Nelson <lewis@lewnelson.com>
2022-12-03 12:35:42 +0000

    day 3 part 1

--- src/days/3_dayThree/index.test.ts
+++ src/days/3_dayThree/index.test.ts
@@ -1,12 +1,31 @@
 import * as dayThree from './';
 import { TestCase } from '~/types';
 
-describe('dayThree', () => {
-  const testCases: TestCase[] = [
-    { description: 'not implemented', input: '', expectedOutput: 'not implemented' },
-  ];
+const inputA = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
 
+const inputB = '';
+
+const inputC = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+vjAwRtyoiPvj`;
+
+describe('dayThree', () => {
   describe('partOne', () => {
+    const testCases: TestCase[] = [
+      { description: 'For the given rucksacks the total is 157', input: inputA, expectedOutput: 157 },
+      { description: 'For no input the total is 0', input: inputB, expectedOutput: 0 },
+      { description: 'When a rucksack has more than one common character only the first common character is used', input: inputC, expectedOutput: 179 },
+    ];
+
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayThree.partOne(input)).toBe(expectedOutput);
     });
--- src/days/3_dayThree/index.ts
+++ src/days/3_dayThree/index.ts
@@ -1,6 +1,33 @@
 // https://adventofcode.com/2022/day/3
 import { Main } from '~/types';
 
+const findCommonCharacters = (a: string, b: string): string[] => {
+  const aChars = a.split('');
+  const bChars = b.split('');
+  const commonChars = aChars.filter((char) => bChars.includes(char));
+  return commonChars;
+};
+
+const findFirstCommonCharacter = (a: string, b: string): string | undefined => {
+  return findCommonCharacters(a, b)[0];
+};
+
+const getNumericalRepresentation = (char: string): number => {
+  const charCode = char.charCodeAt(0);
+  if (charCode >= 97) return charCode - 96;
+  return charCode - 38;
+};
+
 export const partOne: Main = input => {
-  return 'not implemented';
+  return input.split('\n').reduce((sum, line) => {
+    if (!line) return sum;
+    const [compartmentOne, compartmentTwo] = [line.slice(0, line.length / 2), line.slice(line.length / 2)];
+    const commonCharacter = findFirstCommonCharacter(compartmentOne, compartmentTwo);
+    if (!commonCharacter) {
+      return sum;
+    }
+
+    const n = getNumericalRepresentation(commonCharacter);
+    return sum + n;
+  }, 0);
 };
