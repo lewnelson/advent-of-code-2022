9f48343f5d50e0284d7792858c1920dab1ef91d1
Lewis Nelson <lewis@lewnelson.com>
2022-12-06 11:46:36 +0000

    day 4 part 1

--- src/days/4_dayFour/index.test.ts
+++ src/days/4_dayFour/index.test.ts
@@ -1,12 +1,22 @@
 import * as dayFour from './';
 import { TestCase } from '~/types';
 
-describe('dayFour', () => {
-  const testCases: TestCase[] = [
-    { description: 'not implemented', input: '', expectedOutput: 'not implemented' },
-  ];
+const inputA = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`;
 
+const inputB = '';
+
+describe('dayFour', () => {
   describe('partOne', () => {
+    const testCases: TestCase[] = [
+      { description: 'when there are two pairs where the elves section assignments fully overlap', input: inputA, expectedOutput: 2 },
+      { description: 'when there is no input it returns 0', input: inputB, expectedOutput: 0 },
+    ];
+
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayFour.partOne(input)).toBe(expectedOutput);
     });
--- src/days/4_dayFour/index.ts
+++ src/days/4_dayFour/index.ts
@@ -1,6 +1,21 @@
 // https://adventofcode.com/2022/day/4
 import { Main } from '~/types';
 
+const doesAssignmentOverlap = (a: string, b: string): boolean => {
+  const [aStart, aEnd] = a.split('-').map(Number);
+  const [bStart, bEnd] = b.split('-').map(Number);
+  if (aStart <= bStart && aEnd >= bEnd) return true;
+  if (bStart <= aStart && bEnd >= aEnd) return true;
+  return false;
+};
+
 export const partOne: Main = input => {
-  return 'not implemented';
+  const pairs = input.split('\n');
+  const pairsWithFullOverlaps = pairs.reduce((n, pair) => {
+    if (!pair) return n;
+    const [assignmentA, assignmentB] = pair.split(',');
+    return doesAssignmentOverlap(assignmentA, assignmentB) ? n + 1 : n;
+  }, 0);
+
+  return pairsWithFullOverlaps;
 };
