d641173115b364e4af3107ea17674273d94bc311
Lewis Nelson <lewis@lewnelson.com>
2022-12-07 12:47:46 +0000

    day 7 part 1

--- src/days/7_daySeven/index.test.ts
+++ src/days/7_daySeven/index.test.ts
@@ -1,13 +1,37 @@
 import * as daySeven from './';
 import { TestCase } from '~/types';
 
-describe('daySeven', () => {
-  const testCases: TestCase[] = [
-    { description: 'not implemented', input: '', expectedOutput: 'not implemented' },
-  ];
+const inputA = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`;
 
+describe('daySeven', () => {
   describe('partOne', () => {
-    test.each(testCases)('$description', ({ input, expectedOutput }) => {
+    const testCases: Omit<TestCase, 'description'>[] = [
+      { input: inputA, expectedOutput: 95437 },
+    ];
+
+    test.each(testCases)('for the given input the output is $expectedOutput', ({ input, expectedOutput }) => {
       expect(daySeven.partOne(input)).toBe(expectedOutput);
     });
   });
--- src/days/7_daySeven/index.ts
+++ src/days/7_daySeven/index.ts
@@ -1,6 +1,92 @@
 // https://adventofcode.com/2022/day/7
 import { Main } from '~/types';
 
+type Path = string;
+
+interface Item<TType extends 'file' | 'directory'> {
+  type: TType;
+}
+
+interface File extends Item<'file'> {
+  path: Path;
+  name: string;
+  size: number;
+}
+
+interface Directory extends Item<'directory'> {
+  path: Path;
+  name: string;
+}
+
+type FileSystem = Record<Path, File | Directory>;
+
+const joinPaths = (...paths: Path[]): Path => {
+  return paths.join('/').replace(/\/{2}/g, '/');
+};
+
+const resolvePath = (currentPath: Path, instruction: string): string => {
+  if (!currentPath) return instruction;
+  if (instruction === '..') return currentPath.slice(0, currentPath.lastIndexOf('/'));
+  return joinPaths(currentPath, instruction);
+};
+
+const getFilesystem = (input: string): FileSystem => {
+  const filesystem: FileSystem = {};
+  const lines = input.split('\n');
+  let currentPath: Path = '';
+  lines.forEach(line => {
+    switch (true) {
+      case !!line.match(/^\$\scd\s/):
+        currentPath = resolvePath(currentPath, line.slice('$ cd '.length));
+        if (!filesystem[currentPath]) {
+          filesystem[currentPath] = {
+            type: 'directory',
+            path: currentPath,
+            name: currentPath,
+          };
+        }
+        break;
+
+      case !!line.match(/^dir\s/):
+        const directoryName = line.slice('dir '.length);
+        const directoryPath = joinPaths(currentPath, directoryName);
+        filesystem[directoryPath] = {
+          type: 'directory',
+          path: directoryPath,
+          name: directoryName,
+        };
+        break;
+
+      case !!line.match(/^\d+\s/):
+        const [size, fileName] = line.split(' ');
+        const filePath = joinPaths(currentPath, fileName);
+        filesystem[filePath] = {
+          type: 'file',
+          path: filePath,
+          name: fileName,
+          size: parseInt(size),
+        };
+        break;
+    }
+  });
+
+  return filesystem;
+};
+
 export const partOne: Main = input => {
-  return 'not implemented';
+  const filesystem = getFilesystem(input);
+  const [files, directories] = Object.values(filesystem).reduce(([files, directories], item) => {
+    if (item.type === 'file') return [[...files, item], directories];
+    return [files, [...directories, item]];
+  }, [[], []] as [File[], Directory[]]);
+
+  const directorySizes = directories.map(directory => {
+    const filesInDirectory = files.filter(file => file.path.startsWith(directory.path));
+    return filesInDirectory.reduce((total, file) => total + file.size, 0);
+  });
+
+  return directorySizes.reduce((total, size) => {
+    if (size <= 1e5) return total + size;
+    return total;
+  }, 0);
 };
