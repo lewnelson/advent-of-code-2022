ef61b6a1965e11ab914250dd506dbb84d80edfe8
Lewis Nelson <lewis@lewnelson.com>
2022-12-07 11:16:38 +0000

    day 5 part 1

--- src/days/5_dayFive/index.test.ts
+++ src/days/5_dayFive/index.test.ts
@@ -1,12 +1,22 @@
 import * as dayFive from './';
 import { TestCase } from '~/types';
 
-describe('dayFive', () => {
-  const testCases: TestCase[] = [
-    { description: 'not implemented', input: '', expectedOutput: 'not implemented' },
-  ];
+const inputA = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
 
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+describe('dayFive', () => {
   describe('partOne', () => {
+    const testCases: TestCase[] = [
+      { description: 'For the given stack and instructions the output is CMZ', input: inputA, expectedOutput: 'CMZ' },
+    ];
+
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayFive.partOne(input)).toBe(expectedOutput);
     });
--- src/days/5_dayFive/index.ts
+++ src/days/5_dayFive/index.ts
@@ -1,6 +1,85 @@
 // https://adventofcode.com/2022/day/5
 import { Main } from '~/types';
 
+interface ParsedInput {
+  stacks: string[][];
+  instructions: string[];
+}
+
+interface Instruction {
+  move: number;
+  from: number;
+  to: number;
+}
+
+const emptyInput: ParsedInput = { stacks: [], instructions: [] };
+
+const getTotalStacks = (lastLine: string): number => {
+  const matches = lastLine.match(/([\d]+)\s+$/);
+  return matches ? parseInt(matches[1]) : 0;
+};
+
+const buildStacks = (totalStacks: number, lines: string[], stacks: ParsedInput['stacks'] = []): ParsedInput['stacks'] => {
+  if (lines.length === 0) {
+    return stacks.map(stack => {
+      const reversedStack = [...stack];
+      reversedStack.reverse();
+      return reversedStack;
+    });
+  }
+
+  const [nextLine, ...restOfLines] = lines;
+  for (let i = 0; i < totalStacks; i++) {
+    const j = i * 4 - 1;
+    const k =  j + 4;
+    const nextCharSet = nextLine.slice(Math.max(j, 0), k);
+    const nextCharMatch = nextCharSet.match(/([A-Z])/i);
+    if (nextCharMatch && nextCharMatch[1]) {
+      const nextChar = nextCharMatch[1];
+      if (!stacks[i]) stacks[i] = [];
+      stacks[i].push(nextChar);
+    }
+  }
+
+  return buildStacks(totalStacks, restOfLines, stacks);
+};
+
+const parseInstruction = (instructionInput: string): Instruction | null => {
+  const matches = instructionInput.match(/([\d]+)/g);
+  if (!matches) return null;
+  return {
+    move: parseInt(matches[0]),
+    from: parseInt(matches[1]),
+    to: parseInt(matches[2]),
+  };
+};
+
+const parseInput = (input: string): ParsedInput => {
+  const [stacksInput, instructionsInput] = input.split('\n\n');
+  const stacksLines = stacksInput.split('\n');
+  const lastStackLine = stacksLines.pop();
+  if (!lastStackLine) return emptyInput;
+  const totalStacks = getTotalStacks(lastStackLine);
+  const stacks = buildStacks(totalStacks, stacksLines);
+  return { stacks, instructions: instructionsInput.split('\n') };
+};
+
 export const partOne: Main = input => {
-  return 'not implemented';
+  const { stacks, instructions } = parseInput(input);
+  instructions.forEach(line => {
+    const instruction = parseInstruction(line);
+    if (!instruction) return;
+
+    for (let i = 0; i < instruction.move; i++) {
+      const fromStack = stacks[instruction.from - 1];
+      const toStack = stacks[instruction.to - 1];
+      const nextItem = fromStack.pop();
+      if (nextItem) toStack.push(nextItem);
+    }
+  });
+
+  return stacks.reduce((message, stack) => {
+    const item = stack.pop();
+    return `${message}${item ?? ''}`;
+  }, '');
 };
