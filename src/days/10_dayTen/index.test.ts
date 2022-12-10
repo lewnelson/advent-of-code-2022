439ff9053b9ccf4e0570f85091dac59c8d344781
Lewis Nelson <lewis@lewnelson.com>
2022-12-10 14:31:04 +0000

    day 10 part 1

--- src/days/10_dayTen/index.test.ts
+++ src/days/10_dayTen/index.test.ts
@@ -1,12 +1,159 @@
 import * as dayTen from './';
 import { TestCase } from '~/types';
 
-describe('dayTen', () => {
-  const testCases: TestCase[] = [
-    { description: 'not implemented', input: '', expectedOutput: 'not implemented' },
-  ];
+const inputA = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`;
 
+describe('dayTen', () => {
   describe('partOne', () => {
+    const testCases: TestCase[] = [
+      { description: 'for the set of instructions the sum of the signal strenghs is 13140', input: inputA, expectedOutput: 13140 },
+    ];
+
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayTen.partOne(input)).toBe(expectedOutput);
     });
--- src/days/10_dayTen/index.ts
+++ src/days/10_dayTen/index.ts
@@ -2,5 +2,29 @@
 import { Main } from '~/types';
 
 export const partOne: Main = input => {
-  return 'not implemented';
+  const instructions = input.split('\n');
+  let cycleCount = 0;
+  let signalStrength = 0;
+  let x = 1;
+  const tick = (x: number) => {
+    cycleCount++;
+    if ((cycleCount + 20) % 40 === 0) {
+      signalStrength += x * cycleCount;
+    }
+  };
+
+  instructions.forEach(instruction => {
+    switch (true) {
+      case instruction === 'noop':
+        tick(x);
+        break;
+      case instruction.startsWith('addx'):
+        tick(x);
+        tick(x);
+        x += Number(instruction.split(' ')[1]);
+        break;
+    }
+  });
+
+  return signalStrength;
 };
