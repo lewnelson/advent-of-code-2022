529a5d40d6340e742f573c53b708f13379e7935a
Lewis Nelson <lewis@lewnelson.com>
2022-12-12 14:01:05 +0000

    optimise day 12 for performance

--- src/days/12_dayTwelve/index.ts
+++ src/days/12_dayTwelve/index.ts
@@ -3,21 +3,19 @@ import { Main } from '~/types';
 
 type Index = number;
 type StepCount = number;
-type Move = [Index, StepCount];
+type Path = [Index, StepCount];
+
+type CanMoveFromCharToChar = (grid: string, from: number, to: number) => boolean;
 
 interface ShortestPathInput {
   grid: string;
   nColumns: number;
   start: number;
-  end: number;
-  maxPathSize?: number
+  canMoveToChar: CanMoveFromCharToChar;
+  hasReachedEnd: (index: number) => boolean;
 }
 
-const getShortestPath = ({ grid, nColumns, start, end, maxPathSize }: ShortestPathInput) => {
-  const canMoveToChar = (grid: string, from: number, to: number) => {
-    return grid.charCodeAt(to) - grid.charCodeAt(from)  <= 1;
-  };
-
+const getShortestPath = ({ grid, nColumns, start, hasReachedEnd, canMoveToChar }: ShortestPathInput) => {
   const hasVisited = (visited: number[], index: number) => visited.includes(index);
 
   const canMoveLeft = (visited: number[], grid: string) => (index: number) => !hasVisited(visited, index - 1) && index % nColumns !== 0 && canMoveToChar(grid, index, index - 1);
@@ -26,55 +24,45 @@ const getShortestPath = ({ grid, nColumns, start, end, maxPathSize }: ShortestPa
   const canMoveDown = (visited: number[], grid: string) => (index: number) => !hasVisited(visited, index + nColumns) && index < grid.length - nColumns && canMoveToChar(grid, index, index + nColumns);
 
   const visited: number[] = [start];
-  let moves: Move[] = [[start, 0]];
+  let paths: Path[] = [[start, 0]];
   let shortestPath: number = Infinity;
   let prevVisited: number[] = [];
   while (shortestPath === Infinity && prevVisited.length !== visited.length) {
     prevVisited = [...visited];
-    const exhaustedMoveIndexes: number[] = [];
     let i = 0;
-    while (moves[i]) {
-      const currentMove = moves[i];
-      const [currentIndex, steps] = currentMove;
-      if (maxPathSize && steps > maxPathSize) break;
-      if (currentIndex === end) {
+    const nextPaths = [];
+    while (paths[i]) {
+      const currentPath = paths[i];
+      const [currentIndex, steps] = currentPath;
+      if (hasReachedEnd(currentIndex)) {
         shortestPath = steps;
         break;
       }
 
-      const calulateNextMoves: [number, (index: number) => boolean][] = [
-        [currentIndex + 1, canMoveRight(visited, grid)],
-        [currentIndex - nColumns, canMoveUp(visited, grid)],
+      const calculateNextPaths: [number, (index: number) => boolean][] = [
         [currentIndex - 1, canMoveLeft(visited, grid)],
         [currentIndex + nColumns, canMoveDown(visited, grid)],
+        [currentIndex + 1, canMoveRight(visited, grid)],
+        [currentIndex - nColumns, canMoveUp(visited, grid)],
       ];
 
-      const nextMoves = calulateNextMoves.map(([index, canMove]) => canMove(currentIndex) ? index : undefined).filter(index => index !== undefined) as number[];
-      visited.push(...nextMoves);
-      const [nextMove, ...branchedNextMoves] = nextMoves;
-
-      if (nextMove === undefined) {
-        exhaustedMoveIndexes.push(i);
-      } else {
-        branchedNextMoves.forEach(index => {
-          const newMove: Move = [index, steps + 1];
-          moves.push(newMove);
-        });
-
-        currentMove[0] = nextMove;
-        currentMove[1] = steps + 1;
-      }
-
+      const nextPathsIndexes = calculateNextPaths.map(([index, canMove]) => canMove(currentIndex) ? index : undefined).filter(index => index !== undefined) as number[];
+      visited.push(...nextPathsIndexes);
+      nextPaths.push(...nextPathsIndexes.map(index  => [index, steps + 1] as Path));
       i++;
     }
 
-    moves = moves.filter((_, index) => !exhaustedMoveIndexes.includes(index));
+    paths = nextPaths;
   }
 
   return shortestPath;
 };
 
 export const partOne: Main = input => {
+  const canMoveToChar: CanMoveFromCharToChar = (grid, from, to) => {
+    return grid.charCodeAt(to) - grid.charCodeAt(from) <= 1;
+  };
+
   let grid = input.replace(/\n/g, '');
   const start = grid.indexOf('S');
   const end = grid.indexOf('E');
@@ -84,31 +72,28 @@ export const partOne: Main = input => {
     grid,
     nColumns,
     start,
-    end,
+    canMoveToChar,
+    hasReachedEnd: (index: number) => index === end,
   });
 };
 
 export const partTwo: Main = input => {
+  const canMoveToChar: CanMoveFromCharToChar = (grid, from, to) => {
+    return grid.charCodeAt(from) - grid.charCodeAt(to) <= 1;
+  };
+
   const nColumns = input.slice(0, input.indexOf('\n')).length;
   let grid = input.replace(/\n/g, '');
-  const end = grid.indexOf('E');
+  const start = grid.indexOf('E');
   grid = grid.replace(/S/, 'a').replace(/E/, 'z');
-  const startingPositions = grid.split('').reduce((acc, char, index) => {
-    if (char === 'a') acc.push(index);
-    return acc;
-  }, [] as number[]);
 
-  let shortestPath = Infinity;
-  while (startingPositions.length > 0) {
-    const nextPosition = startingPositions.shift()!;
-    shortestPath = Math.min(shortestPath, getShortestPath({
-      grid,
-      nColumns,
-      start: nextPosition,
-      end,
-      maxPathSize: shortestPath,
-    }));
-  }
+  const shortestPath = getShortestPath({
+    grid,
+    nColumns,
+    start,
+    canMoveToChar,
+    hasReachedEnd: (index: number) => grid[index] === 'a',
+  });
 
   return shortestPath;
 };
