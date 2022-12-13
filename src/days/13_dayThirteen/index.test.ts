a1184b4fee67256aff076e040e42170d83ce566a
Lewis Nelson <lewis@lewnelson.com>
2022-12-13 10:36:23 +0000

    day 13 part 1

--- src/days/13_dayThirteen/index.test.ts
+++ src/days/13_dayThirteen/index.test.ts
@@ -1,12 +1,36 @@
 import * as dayThirteen from './';
 import { TestCase } from '~/types';
 
-describe('dayThirteen', () => {
-  const testCases: TestCase[] = [
-    { description: 'not implemented', input: '', expectedOutput: 'not implemented' },
-  ];
+const inputA = `[1,1,3,1,1]
+[1,1,5,1,1]
 
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`;
+
+describe('dayThirteen', () => {
   describe('partOne', () => {
+    const testCases: TestCase[] = [
+      { description: 'for the given packets the sum of the indices of the correctly ordered packets is 13', input: inputA, expectedOutput: 13 },
+    ];
+
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayThirteen.partOne(input)).toBe(expectedOutput);
     });
--- src/days/13_dayThirteen/index.ts
+++ src/days/13_dayThirteen/index.ts
@@ -1,6 +1,46 @@
 // https://adventofcode.com/2022/day/13
 import { Main } from '~/types';
 
+type Order = 'left' | 'right' | 'equal';
+
+const isNumber = (value: unknown): value is number => typeof value === 'number';
+
+const getNumberOrder = (left: number, right: number): Order => {
+  if (left < right) return 'left';
+  if (left > right) return 'right';
+  return 'equal';
+};
+
+const getOrder = (left: unknown, right: unknown): Order => {
+  if (left === undefined && right === undefined) return 'equal';
+  if (left === undefined && right !== undefined) return 'left';
+  if (left !== undefined && right === undefined) return 'right';
+  if (isNumber(left) && isNumber(right)) return getNumberOrder(left, right);
+  if (Array.isArray(left) && Array.isArray(right)) {
+    const maxLength = Math.max(left.length, right.length);
+    for (let i = 0; i < maxLength; i++) {
+      const leftValue = left[i];
+      const rightValue = right[i];
+      const order = getOrder(leftValue, rightValue);
+      if (order !== 'equal') return order;
+    }
+
+    return 'equal';
+  }
+
+  if (Array.isArray(left) && !Array.isArray(right)) return getOrder(left, [right]);
+  if (!Array.isArray(left) && Array.isArray(right)) return getOrder([left], right);
+  throw new Error('Unhandled');
+};
+
 export const partOne: Main = input => {
-  return 'not implemented';
+  const pairs = input.split('\n\n');
+  const sum = pairs.reduce((total, pair, index) => {
+    const pairNumber = index + 1;
+    const [left, right] = pair.split('\n').map(line => JSON.parse(line));
+    const order = getOrder(left, right);
+    return order === 'left' ? total + pairNumber : total;
+  }, 0);
+
+  return sum;
 };
