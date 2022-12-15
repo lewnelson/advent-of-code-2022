549651131c0c2d9108c96f31517b3b80d9f82e25
Lewis Nelson <lewis@lewnelson.com>
2022-12-15 13:30:53 +0000

    day 15 part 1

--- src/days/15_dayFifteen/index.test.ts
+++ src/days/15_dayFifteen/index.test.ts
@@ -1,12 +1,27 @@
 import * as dayFifteen from './';
 import { TestCase } from '~/types';
 
+const inputA = `Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3`;
+
 describe('dayFifteen', () => {
+  describe('partOne', () => {
   const testCases: TestCase[] = [
-    { description: 'not implemented', input: '', expectedOutput: 'not implemented' },
+    { description: 'for the given sensor data the number of positions on row 10 where a beacon cannot possibly be is 26', input: inputA, expectedOutput: 26 },
   ];
 
-  describe('partOne', () => {
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayFifteen.partOne(input)).toBe(expectedOutput);
     });
--- src/days/15_dayFifteen/index.ts
+++ src/days/15_dayFifteen/index.ts
@@ -1,6 +1,70 @@
 // https://adventofcode.com/2022/day/15
 import { Main } from '~/types';
 
+type Coordinate = [number, number];
+type SensorData = {
+  sensor: Coordinate;
+  beacon: Coordinate;
+}
+
+const extractCoordinates = (line: string): Coordinate => {
+  const matches = line.match(/(x=-?[\d]+|y=-?[\d]+)/g);
+  if (!matches) throw new Error(`Invalid input: ${line}`);
+  const coordinates = matches.map(coordinate => coordinate.replace(/(x=|y=)/, '')).map(v => parseInt(v));
+  if (coordinates.length !== 2) throw new Error(`Invalid input: ${line}`);
+  return coordinates as Coordinate;
+};
+
+const getSensorData = (input: string): SensorData[] => {
+  return input.split('\n').map(line => {
+    const [sensor, beacon] = line.split(':');
+    return {
+      sensor: extractCoordinates(sensor),
+      beacon: extractCoordinates(beacon),
+    };
+  });
+};
+
+const getDistance = (a: Coordinate, b: Coordinate): number => {
+  return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+};
+
+const getSensorIndexCoveringCoordinate = (data: SensorData[], coordinate: Coordinate): number => {
+  const sensorData = [...data];
+  for (let i = 0; i < sensorData.length; i++) {
+    const { sensor, beacon } = sensorData[i];
+    const beaconDistance = getDistance(sensor, beacon);
+    const coordinateDistance = getDistance(sensor, coordinate);
+    if (coordinateDistance <= beaconDistance) return i;
+  }
+
+  return -1;
+};
+
+const canBeaconBePlacedAt = (data: SensorData[], coordinate: Coordinate): boolean => {
+  return getSensorIndexCoveringCoordinate(data, coordinate) === -1;
+};
+
+const getBoundaries = (sensorData: SensorData[]): [Coordinate, Coordinate] => {
+  return sensorData.reduce((boundaries, { sensor, beacon }) => {
+    const distance = getDistance(sensor, beacon);
+    return [
+      [Math.min(boundaries[0][0], sensor[0] - distance), Math.min(boundaries[0][1], sensor[1] - distance)],
+      [Math.max(boundaries[1][0], sensor[0] + distance), Math.max(boundaries[1][1], sensor[1] + distance)],
+    ];
+  }, [sensorData[0].sensor, sensorData[0].sensor] as [Coordinate, Coordinate]);
+};
+
 export const partOne: Main = input => {
-  return 'not implemented';
+  const sensorData = getSensorData(input);
+  const [[minX], [maxX, maxY]] = getBoundaries(sensorData);
+  const rowToCheck = maxY >= 2000000 ? 2000000 : 10;
+  const beacons = new Set(sensorData.map(({ beacon }) => beacon.join(',')));
+  let total = 0;
+  for (let x = minX; x <= maxX; x++) {
+    const coordinate = [x, rowToCheck] as Coordinate;
+    if (!beacons.has(coordinate.join(',')) && !canBeaconBePlacedAt(sensorData, coordinate)) total++;
+  };
+
+  return total;
 };
